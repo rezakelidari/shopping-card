a0becec2f35b1f3f12bb9f3a173601d8d83160fe
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-27 21:49:05 +0330

    Making Navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Styles from "./Navbar.module.css";
+import Title from "../Title";
+
+function Navbar() {
+  return (
+    <nav>
+      <Title />
+    </nav>
+  );
+}
+
+export default Navbar;
