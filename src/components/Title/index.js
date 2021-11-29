d769dd5ea287fdf90fc3a7e8d2a3bb29a3001c9b
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-29 21:58:27 +0330

    Updating Title component

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Styles from "./Title.module.css";
 
 function Title() {
-  return <div className={Styles.titleMain}>MyShop</div>;
+  return (
+    <Link to="/">
+      <div className={Styles.titleMain}>MyShop</div>
+    </Link>
+  );
 }
 
 export default Title;
