8904cedb8ead7e47b9c3d6f001cbb3bdf61739dd
Reza Alone <reza.phoenix.1382@gmail.com>
2021-12-01 13:50:52 +0330

    Adding Pages to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Styles from "./styles/App.module.css";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import store from "./states/store";
+import Product from "./pages/Product";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <BrowserRouter>
+      <div className={Styles.appMain}>
+        <Navbar />
+        <Provider store={store}>
+          <Routes>
+            <Route path="/products/:id" element={<Product />} />
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Provider>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
